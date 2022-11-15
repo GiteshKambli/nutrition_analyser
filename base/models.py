d89138a652f48ce33925ce9a54a6b1b003ac215d
Gitesh <90142352+GiteshKambli@users.noreply.github.com>
2022-11-15 21:07:09 +0530

    Update models.py

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class UploadImage(models.Model):
+    caption = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.caption
