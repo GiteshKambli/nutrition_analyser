a49018f9bcc35376e87a66c46bf83ac9078e1079
Rishi More <rishimore102@gmail.com>
2022-11-16 23:41:18 +0530

    Updated views.py

--- food_viser/food_viser/views.py
+++ food_viser/food_viser/views.py
@@ -1,9 +1,6 @@
 from .nutrition import get_nutrition
 from django.shortcuts import render
 from django.core.files.storage import default_storage
-# from tensorflow.keras.utils import load_img
-# from tensorflow.keras.utils import img_to_array
-import numpy as np
 import cv2
 
 
@@ -19,10 +16,7 @@ def scan_label(request):
         file_name_2 = default_storage.save(file_name, f)
         file_url = r'../food_viser/' + default_storage.url(file_name_2)
         print(file_url)
-        # original = load_img(file_url, target_size=(640, 640))
-        # numpy_image = img_to_array(original)
         img = cv2.imread(file_url)
-        # print(numpy_image)
         nutrition = get_nutrition(img, ocr='easyocr')
         print(nutrition)
 
