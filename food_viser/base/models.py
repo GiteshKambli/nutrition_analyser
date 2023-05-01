d57327fc3c8c5b819ab9e53c77cbdafa33276d41
Gitesh <90142352+GiteshKambli@users.noreply.github.com>
2023-05-01 23:11:29 +0530

    Added endpoint for entering user details

--- food_viser/base/models.py
+++ food_viser/base/models.py
@@ -1,14 +1,14 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class NutritionProfile(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    weight = models.IntegerField()
-    age = models.IntegerField()
-    height = models.IntegerField()
-    gender = models.CharField(max_length=6, choices=[('male', 'Male'), ('female', 'Female')])
-    activity_level = models.CharField(max_length=10,
-                                      choices=[('sedentary', 'Sedentary'), ('light', 'Light'), ('moderate', 'Moderate'),
-                                               ('high', 'High')])
-    goal = models.CharField(max_length=8, choices=[('lose', 'Lose'), ('maintain', 'Maintain'), ('gain', 'Gain')])
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# class NutritionProfile(models.Model):
+#     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+#     weight = models.IntegerField()
+#     age = models.IntegerField()
+#     height = models.IntegerField()
+#     gender = models.CharField(max_length=6, choices=[('male', 'Male'), ('female', 'Female')])
+#     activity_level = models.CharField(max_length=10,
+#                                       choices=[('sedentary', 'Sedentary'), ('light', 'Light'), ('moderate', 'Moderate'),
+#                                                ('high', 'High')])
+#     goal = models.CharField(max_length=8, choices=[('lose', 'Lose'), ('maintain', 'Maintain'), ('gain', 'Gain')])
