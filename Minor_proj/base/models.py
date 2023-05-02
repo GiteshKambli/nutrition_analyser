70cab14aa868741ff56a63fbd3f3c3462dd24dff
amitm15 <amitmaity0415@gmail.com>
2023-05-02 13:12:56 +0530

    Finalized user

--- Minor_proj/base/models.py
+++ Minor_proj/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Nutrition(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    weight = models.IntegerField()
+    age = models.IntegerField()
+    height = models.IntegerField()
+    gender = models.CharField(max_length=6)
+    activity_level = models.CharField(max_length=10)
+    goal = models.CharField(max_length=8)
+    food_items = models.CharField(max_length=100)
--- Minor_proj/base/urls.py
+++ Minor_proj/base/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import mainPage, UserLoginView, RegisterPage
+from .views import mainPage, UserLoginView, RegisterPage, User_Profile, NutritionDetail
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', mainPage.as_view(), name='main'),
+    path('nutrition/<slug:user_id>/', NutritionDetail.as_view(), name='nutrition'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('register/', RegisterPage.as_view(), name='register'),
     path('logout/', LogoutView.as_view(next_page='main'), name='logout'),
+    path('nutrition_profile/', User_Profile.as_view(), name='nutrition_profile'),
     # path('scan/', scan_label, name="scan"),
 ]
